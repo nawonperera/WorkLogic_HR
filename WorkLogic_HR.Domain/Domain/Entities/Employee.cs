cefbd5136c1d8556d0ec7dc67268a83b9c3d5cf6
nawonperera <nawon2794@gmail.com>
2026-03-17 18:26:56 +0530

    Created the Domain Class

---
+++ WorkLogic_HR.Domain/Domain/Entities/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace WorkLogic_HR.Core.Domain.Entities;
+
+public class Employee
+{
+    [Key]
+    public Guid Id { get; set; }
+    [StringLength(40)]
+    public string? Name { get; set; }
+    [StringLength(40)]
+    public string? Email { get; set; }
+    [StringLength(40)]
+    public string? Role { get; set; }
+}
