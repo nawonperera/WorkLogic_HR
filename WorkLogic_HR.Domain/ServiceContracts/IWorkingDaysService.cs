5f1504d06644f2f19e955613759f02c65efa4e27
nawonperera <nawon2794@gmail.com>
2026-03-19 00:25:47 +0530

    Created a Working Days Servoce

---
+++ WorkLogic_HR.Domain/ServiceContracts/IWorkingDaysService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkLogic_HR.Core.DTO;
+
+namespace WorkLogic_HR.Core.ServiceContracts;
+
+public interface IWorkingDaysService
+{
+    public int WorkingDays(DateTime startDate, DateTime endDate);
+    public List<PublicHolidayDto> GetHolidaysBetweenDates(DateTime startDate, DateTime endDate);
+}
---
+++ WorkLogic_HR.Domain/Services/WorkingDaysService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkLogic_HR.Core.DTO;
+using WorkLogic_HR.Core.ServiceContracts;
+
+namespace WorkLogic_HR.Core.Services;
+
+public class WorkingDaysService : IWorkingDaysService
+{
+    private readonly IPublicHolidayServiec _publicHolidayService;
+
+    public WorkingDaysService(IPublicHolidayServiec publicHolidayService)
+    {
+        _publicHolidayService = publicHolidayService;
+    }
+    public List<PublicHolidayDto> GetHolidaysBetweenDates(DateTime startDate, DateTime endDate)
+    {
+        return _publicHolidayService.GetSelectedHolidays(startDate, endDate).ToList();
+    }
+
+    public int WorkingDays(DateTime startDate, DateTime endDate)
+    {
+        if (startDate.Date > endDate.Date)
+        {
+            return -1;
+        }
+
+        if (startDate.DayOfWeek == DayOfWeek.Saturday || startDate.DayOfWeek == DayOfWeek.Sunday)
+        {
+            return -1;
+        }
+
+        int workingDays = 0;
+        DateTime currentDate = startDate.Date;
+        List<PublicHolidayDto> publicHolidays = _publicHolidayService.PublicHolidays();
+
+        while (currentDate <= endDate.Date)
+        {
+            if (!publicHolidays.Any(h => h.Date.Date == currentDate))
+            {
+                workingDays++;
+            }
+            currentDate = currentDate.AddDays(1);
+        }
+        return workingDays;
+    }
+}
